9261f365fcfc360a5917084e628f51c8f29d3853
pmharper1 <preslee.harper@yahoo.com>
2023-09-07 10:02:11 -0500

    menu

---
+++ Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameButton : MonoBehaviour
+{
+    public void ExitButton()
+    {
+        Application.Quit();
+        Debug.Log("Game closed");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Alleyway");
+    }
+
+    public void CreditsMenu()
+    {
+        SceneManager.LoadScene("CreditsMenu");
+    }
+
+    public void ControlsMenu()
+    {
+        SceneManager.LoadScene("ControlsMenu");
+    }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}        
